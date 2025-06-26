7f32b24ddbe1e13c188f77a320134d6bb4d331a8
ifuensan <ivanfs@b4os.dev>
2025-06-26 20:41:19 +0200

    Update health check route and clean up unused code

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,20 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder};
-
+use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Responder, web};
+/**
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("world");
     format!("Hello, {}!", name)
 }
+*/
 
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok()
+}
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     HttpServer::new(|| {
         App::new()
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
+            .route("/health_check", web::get().to(health_check))
     })
     .bind("127.0.0.1:8000")?
     .run()
